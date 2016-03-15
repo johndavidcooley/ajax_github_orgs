4838ad720ca45e3470df3ffb6b05cea55bba607e
John Cooley <johndavidcooley@gmail.com>
2016-03-15 13:35:11 -0400

    Final commit.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,23 @@
 // Javascript Entry Point
 
+import $ from 'jquery';
+
 var url = "https://api.github.com/users/wycats/orgs?acces_token=4ad44bd655568433196dc3e53e87663e0860b2a8";
+
+$.ajax(url).then(function(data) {
+	var $app = $('.app');
+
+	data.forEach(function({login, avatar_url}) {
+		 console.log(avatar_url);
+		  $app.append(
+			`
+			<div class="container">
+				<img src="${avatar_url}" alt="">
+				<h3>${login}</h3>
+			</div>
+			`);
+	});
+});
+
+
+// ${avatar_url} ${login}
